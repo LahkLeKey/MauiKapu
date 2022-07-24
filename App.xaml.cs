b376c6f95dc4d7b2adf3bb7a8e521b09d7619784
LahkLeKey <LahkLeKey@gmail.com>
2022-07-23 23:13:59 -0500

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiKapu;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiKapu;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
